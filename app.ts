06e5cda773f079716ff1f724fcb7f77d36385562
campishome <campnapat007@gmail.com>
2024-03-16 22:42:16 +0700

    my first commit

---
+++ app.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import { router as index } from "./API/index";
+import { router as user } from "./API/user";
+import { router as main } from "./API/main";
+import { router as image } from "./API/image";
+import bodyParser from "body-parser";
+
+export const app = express();
+app.use(
+    cors({
+      origin: "*",
+    })
+  );
+app.use(bodyParser.json());
+app.use("/", index);
+app.use("/user", user);
+app.use("/main", main);
+app.use("/image", image);
